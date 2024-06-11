89f14c4b837187e3786d64e6fd17a39208f545f9
Yusefcito <162405520+Yusefcito@users.noreply.github.com>
2024-06-11 22:35:00 +0000

    bitacoras/check

--- cypress/e2e/modulos/conserjeria/bitacoras.cy.js
+++ cypress/e2e/modulos/conserjeria/bitacoras.cy.js
@@ -28,33 +28,33 @@ describe("Suite de bitácora", () => {
     cy.url().should("eq", "https://qa.stag.edipro.cl/bitacoras");
   });
 
-  // //CP-1 - Validar contenido del módulo Bitácoras.
-  // it("Revisar modulo Conserjería", () => {
-  //   //Esto revisará el contenido del módulo
-  //   cy.get("h2.main-content-title.tx-24.mg-b-5")
-  //     .contains("Bitácoras")
-  //     .should("exist");
-  //   //botones disponibles
-  //   cy.get(".btn.btn-primary.btn-icon-text[href='/bitacoras/new']")
-  //     .contains("Registrar")
-  //     .should("exist");
-  //   cy.get(".btn.btn-info.btn-icon-text").contains("Buscar").should("exist");
-  //   cy.get(".btn.btn-dark.btn-icon-text").contains("Excel").should("exist");
-  //   cy.get(".btn.btn-warning.btn-icon-text")
-  //     .contains("Acciones")
-  //     .should("exist");
+  //CP-1 - Validar contenido del módulo Bitácoras.
+  it("Revisar modulo Conserjería", () => {
+    //Esto revisará el contenido del módulo
+    cy.get("h2.main-content-title.tx-24.mg-b-5")
+      .contains("Bitácoras")
+      .should("exist");
+    //botones disponibles
+    cy.get(".btn.btn-primary.btn-icon-text[href='/bitacoras/new']")
+      .contains("Registrar")
+      .should("exist");
+    cy.get(".btn.btn-info.btn-icon-text").contains("Buscar").should("exist");
+    cy.get(".btn.btn-dark.btn-icon-text").contains("Excel").should("exist");
+    cy.get(".btn.btn-warning.btn-icon-text")
+      .contains("Acciones")
+      .should("exist");
 
-  //   cy.get(".btn.ripple.btn-primary[href='/bitacoras/new']")
-  //     .contains("Agregar Bitacora")
-  //     .should("exist");
-  //   //fin de botones
+    cy.get(".btn.ripple.btn-primary[href='/bitacoras/new']")
+      .contains("Agregar Bitacora")
+      .should("exist");
+    //fin de botones
 
-  //   cy.get("h6.main-content-label.mb-1")
-  //     .contains("Sin registros de bitacora para mostrar ")
-  //     .should("exist");
-  // });
+    cy.get("h6.main-content-label.mb-1")
+      .contains("Sin registros de bitacora para mostrar ")
+      .should("exist");
+  });
 
-  //CP - 2 - Validar form registrar
+  // // CP - 2 - Validar form registrar
   it("Botón Registar", () => {
     cy.get(".btn.btn-primary.btn-icon-text[href='/bitacoras/new']")
       .contains("Registrar")
@@ -129,5 +129,130 @@ describe("Suite de bitácora", () => {
 
     // Verificar la presencia del botón Guardar
     cy.get("input.btn.ripple.btn-main-primary").should("exist");
+  });
+
+  // // CP - 3 - BOTON BUSCAR
+  it("botón buscar", () => {
+    cy.get(".btn.btn-info.btn-icon-text").should("exist").click();
+
+    cy.get(".modal-title").should(
+      "contain.text",
+      "Búsqueda de Bitácora de Novedades"
+    );
+
+    cy.get(".modal-body");
+    cy.get("#q_filter_by_date").should("exist");
+    cy.get("#q_numero_eq").should("exist");
+    cy.get("#q_comentario_cont").should("exist");
+
+    cy.get("#q_tipo_bitacora_id_in").invoke("select2", "open");
+    cy.get(".select2-results__options").contains("Cambio de Turno").click();
+    cy.get("#q_tipo_bitacora_id_in").invoke("select2", "open");
+    cy.get(".select2-results__options").contains("Colación").click();
+    cy.get("#q_tipo_bitacora_id_in").invoke("select2", "open");
+    cy.get(".select2-results__options").contains("Encomienda").click();
+    cy.get("#q_tipo_bitacora_id_in").invoke("select2", "close");
+
+    cy.get(
+      ".row.row-xs.align-items-center.mg-b-10.select.optional.q_user_id_in"
+    ).should("exist");
+
+    cy.get(
+      ".row.row-xs.align-items-center.mg-b-10.select.optional.q_departamento_id_in"
+    ).click();
+    cy.get("#q_departamento_id_in").should("exist");
+    cy.get(
+      ".select2-results__option.select2-results__option--highlighted"
+    ).click();
+
+    // -----------------------------
+
+    // Abre el combo box
+
+    cy.get("#select2-q_destacado_true-container").click();
+
+    cy.get("ul.select2-results__options").within(() => {
+      cy.contains("li", "Si").should("exist");
+      cy.contains("li", "No").should("exist");
+    });
+
+    cy.get("#select2-q_destacado_true-container").click();
+
+    cy.get(".modal-footer .btn.btn-danger")
+      .contains("Cerrar")
+      .should("exist")
+
+    // Verifica que el botón "Buscar" exista y sea visible
+    cy.get(".modal-footer .btn.btn-primary.btn-modal-form")
+      .contains("Buscar")
+      .should("exist")
+  });
+
+  it("botón excel", () => {
+    cy.intercept("GET", "/bitacoras.xlsx", {
+      statusCode: 200,
+      body: "Interceptado",
+    }).as("descargaExcel");
+
+    // Verifica que el enlace de descarga de Excel exista y sea visible
+    cy.get('a[href="/bitacoras.xlsx"].btn.btn-dark.btn-icon-text')
+      .should("exist")
+      .and("be.visible");
+
+    // Verifica que el enlace contenga el icono correcto
+    cy.get(
+      'a[href="/bitacoras.xlsx"].btn.btn-dark.btn-icon-text i.fa.fa-file-excel-o'
+    )
+      .should("exist")
+      .and("be.visible");
+
+    // Verifica que el texto del enlace contenga "Excel"
+    cy.get('a[href="/bitacoras.xlsx"].btn.btn-dark.btn-icon-text').contains(
+      "Excel"
+    );
+
+    // Verifica el href del enlace
+    cy.get('a[href="/bitacoras.xlsx"]').should(
+      "have.attr",
+      "href",
+      "/bitacoras.xlsx"
+    );
+  });
+
+  it("botón acciones/bitacora_type", () => {
+    // Verifica que el botón exista y sea visible
+    cy.get('button.btn.btn-warning.btn-icon-text[data-bs-toggle="dropdown"]')
+      .should("exist")
+      .and("be.visible");
+
+    // Verifica que el botón contenga el texto "Acciones"
+    cy.get(
+      'button.btn.btn-warning.btn-icon-text[data-bs-toggle="dropdown"]'
+    ).contains("Acciones");
+
+    cy.get(".btn.btn-warning.btn-icon-text").click();
+    cy.get(".dropdown-menu.show").should("exist");
+    cy.get(".dropdown-item[href='/tipo_bitacoras']").click();
+
+    cy.url().should("eq", "https://qa.stag.edipro.cl/tipo_bitacoras");    
+  });
+
+
+  it("botón acciones/encomienda", () => {
+    // Verifica que el botón exista y sea visible
+    cy.get('button.btn.btn-warning.btn-icon-text[data-bs-toggle="dropdown"]')
+      .should("exist")
+      .and("be.visible");
+
+    // Verifica que el botón contenga el texto "Acciones"
+    cy.get(
+      'button.btn.btn-warning.btn-icon-text[data-bs-toggle="dropdown"]'
+    ).contains("Acciones");
+
+    cy.get(".btn.btn-warning.btn-icon-text").click();
+    cy.get(".dropdown-menu.show").should("exist");
+    cy.get(".dropdown-item[href='/encomiendas']").click();
+
+    cy.url().should("eq", "https://qa.stag.edipro.cl/encomiendas");    
   });
 }); //acá se cierra el suite ojito:o
